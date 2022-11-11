7127d37851afdb330c3531076a1d6000727f1eaf
mariamnozadze <mariamnozadze97@yahoo.com>
2022-11-11 21:39:00 +0400

    - Using modulus operator

---
+++ PHP and Algorithmic Thinking/tasks_03.php
@@ -0,0 +1,9 @@
+<?php
+//The modulus ( % ) operator
+$num1 = 10;
+$num2 = 4;
+$q = (int)$num1 / $num2;
+$r = $num1 % $num2;
+
+echo "Integer Quotient: ", $q, " Integer Remainder: ", $r;
+//Integer Quotient: 2.5 Integer Remainder: 2
